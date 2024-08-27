3ac48b08dc766d2a301284ed2a80fa3c7dd24391
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-27 13:43:52 -0700

    Implement function getComputerChoice()
    This function chooses randomly between the 3 options for the game.
    It is an essential component for the game to work.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+function getComputerChoice() {
+    // Choose a number from 0-2 (0, 1, or 2). Note we dont need to break because each case returns.
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            // Should never happen.
+            return "Something went wrong."
+    }
+}
+
+console.log(getComputerChoice()); 
